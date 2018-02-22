e680d6de90b2ab29a3ba78ff33e06d5bf7f2a58a
schristiann <36682425+schristiann@users.noreply.github.com>
2018-02-21 22:24:22 -0500

    Add files via upload
    this goes under "app" package

---
+++ GridTest.java
@@ -0,0 +1,35 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import view.GridController;
+
+import java.io.IOException;
+
+public class GridTest extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(
+                getClass().getResource("/view/grid.fxml"));
+        AnchorPane root = loader.load();
+        GridController gridController = loader.getController();
+        gridController.makeGrid(101);
+
+        Scene scene = new Scene(root, 1000, 600);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Grid Test");
+        primaryStage.show();
+    }
+
+
+}
